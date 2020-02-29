bfbd3ba6497cbb7228e8ffdebfea62d1cbed420b
SamanW <saman_waruka@cmu.ac.th>
2020-03-01 06:16:27 +0700

    use diversity algorithm to cal fibonacci

--- part6-remote-procedure-call/rpc_server.js
+++ part6-remote-procedure-call/rpc_server.js
@@ -32,14 +32,32 @@ amqp.connect('amqp://guest:guest@rabbitmq:5672', function(error0, connection) {
   });
 });
 
+let memory = [0,1];
 function fibonacci(n) {
+  // 1st solution :  pure recursive
   // slow and use too much resource way
   // if (n == 0 || n == 1)
   //   return n;
   // else
   //   return fibonacci(n - 1) + fibonacci(n - 2);
 
-  // faster way and use resource less than recursive way
+  // 2nd solution : dynamic programming
+  // if (memory[n]) {
+  //   return memory[n];
+  // }
+
+  // if( n <= 2) {
+  //   return 1;
+  // }
+
+  // else {
+  //   let f =  fibonacci(n - 1) + fibonacci(n - 2);
+  //   memory[n] = f;
+  //   return f;
+  // }
+
+  // 3rd solution : loop through n 
+  // faster way and use resource less than recursive in 1st solution way
   let current = 0;
   let after = 1;
   for ( let i = 0 ; i < n; i++) {
